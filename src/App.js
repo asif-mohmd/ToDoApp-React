7d16e919d45bb9fcf41f186c6c1def004f8d5ca0
Asif Muhammed <asifasifpsps@gmail.com>
2023-07-30 17:10:06 +0530

    todo code formatted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [toDos, setToDos] = useState([])
+  const [toDo, setToDo] = useState('')
   return (
     <div className="app">
       <div className="mainHeading">
@@ -11,19 +14,46 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+
+        <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={() => setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
+
       </div>
+
       <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
+        {toDos.map((obj) => {
+
+          return (<div className="todo">
+            <div className="left">
+              <input onChange={(e) => {
+                console.log(e.target.checkedimage.png)
+                console.log(obj)
+                setToDos(toDos.filter(obj2 => {
+                  if (obj2.id === obj.id) {
+                    obj2.status = e.target.checked
+                  }
+                  return obj2
+                }))
+              }} value={obj.status} type="checkbox" name="" id="" />
+              <p>{obj.text}</p>
+            </div>
+            <div className="right">
+              <i className="fas fa-times"></i>
+            </div>
           </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+          )
+        })
+        }
+
+        {toDos.map((obj) => {
+          if (obj.status) {
+            return (<h1>{obj.text}</h1>)
+          }
+          return null
+        })}
+
+
+
       </div>
     </div>
   );
