dae700920463d8377f1132f830290373a97d8bee
Asif Muhammed <asifasifpsps@gmail.com>
2023-07-31 18:56:25 +0530

    todo app code formatted

--- src/App.js
+++ src/App.js
@@ -45,16 +45,12 @@ function App() {
         })
         }
 
-        
-
         {toDos.map((obj) => {
           if (obj.status) {
             return (<h1>{obj.text}</h1>)
           }
           return null
         })}
-
-
 
       </div>
     </div>
